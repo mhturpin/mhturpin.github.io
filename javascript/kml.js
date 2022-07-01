154b9dbc848090ff42b4c52b26e845d065b8f8b7
mhturpin <mturpin@medicomhealth.com>
2022-07-01 15:48:34 -0500

    Make style optional, change colors

--- javascript/kml.js
+++ javascript/kml.js
@@ -21,26 +21,33 @@ function processGeojson() {
 }
 
 function createKmlDoc(contents) {
-  let parser = new DOMParser();
+  let styles = '';
+
+  if (document.getElementById('include-zone-colors').checked) {
+    styles = createZoneColorStyles();
+  }
+
   let kmlString = `<?xml version="1.0" encoding="UTF-8"?>
     <kml xmlns="http://www.opengis.net/kml/2.2">
       <Document>
-        ${createZoneColorStyles()}
+        ${styles}
         ${contents}
       </Document>
     </kml>
   `;
 
+  let parser = new DOMParser();
   return parser.parseFromString(kmlString, 'text/xml');
 }
 
 // https://datatracker.ietf.org/doc/html/rfc7946
 // Possible types: Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon, GeometryCollection, Feature, FeatureCollection
 function createPlacemark(feature) {
+  let props = feature['properties'];
   let contents = '';
 
   if (feature['geometry']['type'] === 'Polygon') {
-    contents = createPolygon(feature['geometry']['coordinates'], feature['properties']['ZONE']);
+    contents = createPolygon(feature['geometry']['coordinates'], props['ZONE']);
   } else if (feature['geometry']['type'] === 'MultiPolygon') {
     contents = createMultiGeometry(feature['geometry']['coordinates']);
   } else {
@@ -49,10 +56,24 @@ function createPlacemark(feature) {
     document.getElementById('errors').appendChild(p);
   }
 
+  let style = '';
+
+  if (document.getElementById('include-zone-colors').checked) {
+    style = `<styleUrl>#${props['ZONE']}Color</styleUrl>`;
+  }
+
   return `
     <Placemark>
-      <name>${feature['properties']['ZONE']}</name>
-      <styleUrl>#${feature['properties']['ZONE']}Color</styleUrl>
+      ${style}
+      <name>${props['ZONE']}</name>
+      <description>
+        <![CDATA[
+          <h1>Properties:</h1>
+          <ul>
+            ${Object.keys(props).map(k => `<li>${k}: ${props[k]}</li>`)}
+          </ul>
+        ]]>
+      </description>
       ${contents}
     </Placemark>
   `;
@@ -97,34 +118,36 @@ function createMultiGeometry(coordinates) {
   `;
 }
 
+// USDA Zone Colors
+// https://pdi.scinet.usda.gov/phzm/md/All_states_halfzones_poster_rgb_300dpi.pdf
 function createZoneColorStyles() {
   let zoneColors = {
-    '1aColor': '80dac9cd',
-    '1bColor': '80c7b6bc',
-    '2aColor': '80ac9da6',
-    '2bColor': '80bdb2de',
-    '3aColor': '80ae97d0',
-    '3bColor': '809c84c1',
-    '4aColor': '80a4729f',
-    '4bColor': '809c6d66',
-    '5aColor': '80b88a71',
-    '5bColor': '80b59c5c',
-    '6aColor': '80648b51',
-    '6bColor': '806d9d7c',
-    '7aColor': '807cb4aa',
-    '7bColor': '8081c3ca',
-    '8aColor': '808fcfe8',
-    '8bColor': '806ec2e5',
-    '9aColor': '8066add2',
-    '9bColor': '807eb7ea',
-    '10aColor': '80519ddf',
-    '10bColor': '803f82d4',
-    '11aColor': '803c6dcf',
-    '11bColor': '80668dd7',
-    '12aColor': '80536dc3',
-    '12bColor': '804463ab',
-    '13aColor': '804a74a7',
-    '13bColor': '80678db9'
+    '1aColor': '80ffd6d7',
+    '1bColor': '80f2c4c4',
+    '2aColor': '80d9abab',
+    '2bColor': '80ebb0eb',
+    '3aColor': '80eb91e0',
+    '3bColor': '80db7dcf',
+    '4aColor': '80ff6ba8',
+    '4bColor': '80ed755b',
+    '5aColor': '80ffa173',
+    '5bColor': '80e0c95d',
+    '6aColor': '8046ba48',
+    '6bColor': '8056c777',
+    '7aColor': '8069d6ad',
+    '7bColor': '8070dbcf',
+    '8aColor': '8085daed',
+    '8bColor': '8057cbeb',
+    '9aColor': '804fb6dc',
+    '9bColor': '8078b6f4',
+    '10aColor': '80369bec',
+    '10bColor': '801f78e6',
+    '11aColor': '801e56e6',
+    '11bColor': '806485e8',
+    '12aColor': '804e62d4',
+    '12bColor': '802850b4',
+    '13aColor': '802e6fab',
+    '13bColor': '805d91c4'
   }
 
   return Object.keys(zoneColors).map(id => createColorStyle(id, zoneColors[id])).join('');
@@ -170,47 +193,15 @@ function showDowloadButton(name, contents) {
   button.disabled = false;
 }
 
+// let placemark = kmlDoc.createElement('Placemark');
+// let polygon = kmlDoc.createElement('Polygon');
+// let outerBoundaryIs = kmlDoc.createElement('outerBoundaryIs');
+// let linearRing = kmlDoc.createElement('LinearRing');
+// let coordinates = kmlDoc.createElement('coordinates');
 
-    // let placemark = kmlDoc.createElement('Placemark');
-    // let polygon = kmlDoc.createElement('Polygon');
-    // let outerBoundaryIs = kmlDoc.createElement('outerBoundaryIs');
-    // let linearRing = kmlDoc.createElement('LinearRing');
-    // let coordinates = kmlDoc.createElement('coordinates');
+// coordinates.textContent = feature['geometry']['coordinates'][0].map(pair => pair.join()).join('\n');
 
-    // coordinates.textContent = feature['geometry']['coordinates'][0].map(pair => pair.join()).join('\n');
-
-    // linearRing.appendChild(coordinates);
-    // outerBoundaryIs.appendChild(linearRing);
-    // polygon.appendChild(outerBoundaryIs);
-    // placemark.appendChild(polygon);
-
-// USDA Zone Colors
-// https://pdi.scinet.usda.gov/phzm/md/All_states_halfzones_poster_cmyk_300dpi.pdf
-// rgb, hex (rrggbb), kml (aabbggrr)
-// 1a: 205, 201, 218 = #cdc9da = 80dac9cd
-// 1b: 188, 182, 199 = #bcb6c7 = 80c7b6bc
-// 2a: 166, 157, 172 = #a69dac = 80ac9da6
-// 2b: 222, 178, 189 = #deb2bd = 80bdb2de
-// 3a: 208, 151, 174 = #d097ae = 80ae97d0
-// 3b: 193, 132, 156 = #c1849c = 809c84c1
-// 4a: 159, 114, 164 = #9f72a4 = 80a4729f
-// 4b: 102, 109, 156 = #666d9c = 809c6d66
-// 5a: 113, 138, 184 = #718ab8 = 80b88a71
-// 5b: 92, 156, 181 = #5c9cb5 = 80b59c5c
-// 6a: 81, 139, 100 = #518b64 = 80648b51
-// 6b: 124, 157, 109 = #7c9d6d = 806d9d7c
-// 7a: 170, 180, 124 = #aab47c = 807cb4aa
-// 7b: 202, 195, 129 = #cac381 = 8081c3ca
-// 8a: 232, 207, 143 = #e8cf8f = 808fcfe8
-// 8b: 229, 194, 110 = #e5c26e = 806ec2e5
-// 9a: 210, 173, 102 = #d2ad66 = 8066add2
-// 9b: 234, 183, 126 = #eab77e = 807eb7ea
-// 10a: 223, 157, 81 = #df9d51 = 80519ddf
-// 10b: 212, 130, 63 = #d4823f = 803f82d4
-// 11a: 207, 109, 60 = #cf6d3c = 803c6dcf
-// 11b: 215, 141, 102 = #d78d66 = 80668dd7
-// 12a: 195, 109, 83 = #c36d53 = 80536dc3
-// 12b: 171, 99, 68 = #ab6344 = 804463ab
-// 13a: 167, 116, 74 = #a7744a = 804a74a7
-// 13b: 185, 141, 103 = #b98d67 = 80678db9
-
+// linearRing.appendChild(coordinates);
+// outerBoundaryIs.appendChild(linearRing);
+// polygon.appendChild(outerBoundaryIs);
+// placemark.appendChild(polygon);
