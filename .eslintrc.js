943ad785a47e013df946bfab6cd4ed90a3e6328f
mhturpin <mturpin@medicomhealth.com>
2023-01-21 13:30:16 -0600

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'max-len': 'off'
+  },
+};
